b2d3bdf72e03824a07bfc078791c02f79a9d541f
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-02 11:40:06 -0600

    feat: server mock added to get a placeholde to figure out how it is supposed to be

--- null_services_frontend/src/app/modules/chat/pages/server/server/server.ts
+++ null_services_frontend/src/app/modules/chat/pages/server/server/server.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-server',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './server.html',
   styleUrl: './server.css',
 })
-export class Server {
+export class Server implements OnInit{
+
+  currentServerId: String | null = null;
+  serverName: string = 'Servidor de Prueba'
+
+  textChannels = [
+    {id: '1', name: 'general'},
+    {id: '2', name: 'anuncios'},
+    {id: '3', name: 'off-topics'}
+  ];
+
+  activeChannelId: string = '1';
+
+  isMembersListOpen: boolean = true;
+
+  constructor(
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+      this.route.paramMap.subscribe(param => {
+        this.currentServerId = param.get('serverId')
+      })
+  }
+
+  selectChannel(channelId: string): void{
+    this.activeChannelId = channelId
+  }
+
+  toggleMembersList(): void {
+    this.isMembersListOpen = !this.isMembersListOpen;
+  }
 
 }
