72a3d462705c25d2f5d1b0334f0d923b8470bb78
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-04 22:44:33 -0600

    feat: new project added, GATEWAY structure in process

---
+++ null_services_gateway/src/main/java/com/fer/null_services_gateway/NullServicesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fer.null_services_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullServicesGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullServicesGatewayApplication.class, args);
+	}
+
+}
---
+++ null_services_gateway/src/test/java/com/fer/null_services_gateway/NullServicesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.null_services_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullServicesGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
