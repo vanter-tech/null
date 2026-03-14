3b8cc468024786edd606cf99073ec87570c1f4ef
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-13 19:39:37 -0600

    feat: member profile pop-up in server sidebar working but not done yet, some issues

--- null_services_frontend/src/app/modules/chat/components/member-sidebar/member-sidebar/components/member-profile-pop-up/member-profile-pop-up/member-profile-pop-up.ts
+++ null_services_frontend/src/app/modules/chat/components/member-sidebar/member-sidebar/components/member-profile-pop-up/member-profile-pop-up/member-profile-pop-up.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-member-profile-pop-up',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './member-profile-pop-up.html',
   styleUrl: './member-profile-pop-up.css',
 })
 export class MemberProfilePopUp {
+
+@Input({required: true}) member: any;
+@Output() close = new EventEmitter<void>();
+@Input() topPosition: number = 0;
 
 }
--- null_services_frontend/src/app/modules/chat/components/member-sidebar/member-sidebar/member-sidebar.ts
+++ null_services_frontend/src/app/modules/chat/components/member-sidebar/member-sidebar/member-sidebar.ts
@@ -1,16 +1,38 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule, UpperCasePipe
- } from '@angular/common';
+import { CommonModule, UpperCasePipe} from '@angular/common';
+
+import { MemberProfilePopUp } from './components/member-profile-pop-up/member-profile-pop-up/member-profile-pop-up';
 
 @Component({
   selector: 'app-member-sidebar',
   standalone: true,
-  imports: [CommonModule, UpperCasePipe],
+  imports: [CommonModule, UpperCasePipe, MemberProfilePopUp],
   templateUrl: './member-sidebar.html',
   styleUrl: './member-sidebar.css',
 })
 export class MemberSidebar {
 
-  @Input() members: any[] = []
+ @Input() members: any[] = []
+  selectedMember: any | null = null;
+  popupTop: number = 0; 
+
+  
+  toggleProfile(member: any, event: MouseEvent){
+    if(this.selectedMember === member){
+      this.selectedMember = null;
+    } else {
+      this.selectedMember = member;
+      
+      
+      const target = event.currentTarget as HTMLElement;
+      const rect = target.getBoundingClientRect();
+    
+      this.popupTop = rect.top - 1.5; 
+    }
+  }
+
+  closeProfile(){
+    this.selectedMember = null;
+  }
 
 }
