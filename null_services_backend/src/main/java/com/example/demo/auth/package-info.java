445af20b7ce483a667a88a3ca06564d9ed89923a
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-15 10:56:21 -0600

    fix: (chekstyle errors) authenticationController

---
+++ null_services_backend/src/main/java/com/example/demo/auth/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Proporciona las clases y controladores necesarios para la autenticación.
+ * Aquí se maneja el registro de usuarios, inicio de sesión y validación de tokens JWT.
+ */
+
+package com.example.demo.auth;
