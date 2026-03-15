3f8203865395f268f6da206422c4474502c76c63
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-15 11:05:36 -0600

    fix: using individuals imports instead .*

--- null_services_backend/src/main/java/com/example/demo/auth/AuthenticationController.java
+++ null_services_backend/src/main/java/com/example/demo/auth/AuthenticationController.java
@@ -6,7 +6,15 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Controlador REST que gestiona el acceso y la identidad en la aplicación.
