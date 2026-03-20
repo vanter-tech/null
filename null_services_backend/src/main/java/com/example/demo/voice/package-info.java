cf83506a630b079ff18167da5ee4c40a279f27c5
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 22:57:53 -0600

    fix: (Checkstyle) package-info - Done.

--- null_services_backend/src/main/java/com/example/demo/voice/package-info.java
+++ null_services_backend/src/main/java/com/example/demo/voice/package-info.java
@@ -1,1 +1,9 @@
+/**
+ * Gestiona la lógica de comunicación por voz
+ * en tiempo real mediante LiveKit.
+ * Proporciona los servicios,
+ * controladores y modelos necesarios para la generación
+ * de tokens de acceso y la representación de
+ * participantes en salas WebRTC.
+ */
 package com.example.demo.voice;
