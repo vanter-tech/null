5b3c3c0d0659c5e9d9dada81b90c20934176d0be
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 19:46:25 -0600

    fix: (Checkstyle) package-info - Done.

--- null_services_backend/src/main/java/com/example/demo/security/package-info.java
+++ null_services_backend/src/main/java/com/example/demo/security/package-info.java
@@ -1,6 +1,9 @@
 /**
- * Contiene la configuración principal y los componentes de seguridad de la aplicación.
- * Implementa la autenticación y autorización mediante JSON Web Tokens (JWT),
- * filtros personalizados y políticas de control de acceso a los endpoints.
+ * Contiene la configuración principal y los
+ * componentes de seguridad de la aplicación.
+ * Implementa la autenticación y autorización
+ * mediante JSON Web Tokens (JWT),
+ * filtros personalizados y políticas de control de
+ * acceso a los endpoints.
  */
 package com.example.demo.security;
