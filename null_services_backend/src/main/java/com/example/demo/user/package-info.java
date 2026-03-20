419c8ba6b679912e06eaa2aff337d8611da13db1
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 21:37:48 -0600

    fix: (Checkstyle) package-info - Done.

--- null_services_backend/src/main/java/com/example/demo/user/package-info.java
+++ null_services_backend/src/main/java/com/example/demo/user/package-info.java
@@ -1,1 +1,9 @@
+/**
+ * Contiene las entidades, repositorios y servicios
+ * principales para la gestión de usuarios.
+ * Representa el dominio central de la aplicación
+ * manejando la identidad,
+ * el estado de conexión en tiempo real y la
+ * integración con la seguridad del sistema.
+ */
 package com.example.demo.user;
