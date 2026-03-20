a03b43b48bed71d87b26bcb8ddf10e27efa4bc97
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-19 20:42:04 -0600

    fix: (Checkstyle) Package-info - Done.

--- null_services_backend/src/main/java/com/example/demo/server/package-info.java
+++ null_services_backend/src/main/java/com/example/demo/server/package-info.java
@@ -1,1 +1,9 @@
+/**
+ * Contiene las entidades, controladores y servicios
+ * para la gestión de servidores y canales.
+ * Representa el núcleo de las comunidades
+ * dentro de la plataforma, permitiendo
+ * a los usuarios crear, unirse, organizar y administrar
+ * espacios de comunicación.
+ */
 package com.example.demo.server;
